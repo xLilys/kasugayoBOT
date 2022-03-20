8babcf29bcb736719197578142d9fc15669638cc
yuuki <yuuki617phone@gmail.com>
2022-03-20 10:58:46 +0900

    サーバー参加をログに通知

--- main/app.js
+++ main/app.js
@@ -10,6 +10,12 @@ client.on('ready',()=>{
     console.log(`${client.user.tag}`);
 });
 
+client.on("guildCreate",guild =>{
+    console.log("registered at \n" + guild.name + '\n' +guild.id);
+});
+
+
+//テスト用
 client.on('messageCreate',message =>{
     if(message.author.bot)return;
     if(message.content.includes('ks')){
